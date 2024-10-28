80f6b664cc4475664b196b3f1ff88138042d9ff6
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-28 19:02:00 +0300

    task 0 fixx

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,9 +1,5 @@
 import $ from 'jquery';
 
-const firstParagraph = $('<p>').text('Holberton Dashboard');
-const secondParagraph = $('<p>').text('Dashboard data for the students');
-const thirdParagraph = $('<p>').text('Copyright - Holberton School');
-
-$('body').append(firstParagraph, secondParagraph, thirdParagraph);
-
-console.log('hello');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
