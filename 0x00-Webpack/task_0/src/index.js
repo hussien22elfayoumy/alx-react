354879b22ab4ca62f7099aaa7ba3026e03ee4a17
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-28 18:58:35 +0300

    task 000

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -5,3 +5,5 @@ const secondParagraph = $('<p>').text('Dashboard data for the students');
 const thirdParagraph = $('<p>').text('Copyright - Holberton School');
 
 $('body').append(firstParagraph, secondParagraph, thirdParagraph);
+
+console.log('hello');
