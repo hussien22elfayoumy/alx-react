af57aad92eb93529b4667885f862a521ce97d2cb
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-28 18:53:25 +0300

    task 0

---
+++ task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+const firstParagraph = $('<p>').text('Holberton Dashboard');
+const secondParagraph = $('<p>').text('Dashboard data for the students');
+const thirdParagraph = $('<p>').text('Copyright - Holberton School');
+
+$('body').append(firstParagraph, secondParagraph, thirdParagraph);
