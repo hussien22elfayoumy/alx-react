5d74354dae437810b82446a8ea10223c70c8f3b8
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-28 20:39:17 +0300

    lll

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -13,5 +13,4 @@ function updateCounter() {
   count++;
   $('#count').html(`${count} clicks on the button`);
 }
-
-$('button').on('click', _.debounce(updateCounter));
+$('button').on('click', _.debounce(updateCounter, 500));
