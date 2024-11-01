4fd6183b3ff268b9caaf1be5de81f7bf9b2de03a
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-11-01 10:49:10 +0200

    task -8

---
+++ 0x01-react_intro/task_4/src/Notifications.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from './utils';
+
+export default function Notifications() {
+  return (
+    <div
+      className='Notifications'
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      <div>
+        <p>Here is the list of notifications</p>
+        <ul>
+          <li data-priority='default'>New course available</li>
+          <li data-priority='urgent'>New resume available</li>
+          <li data-priority='urgent'>{getLatestNotification()}</li>
+        </ul>
+      </div>
+      <button aria-label='close' style={{ alignSelf: 'start' }}>
+        x
+      </button>
+    </div>
+  );
+}
