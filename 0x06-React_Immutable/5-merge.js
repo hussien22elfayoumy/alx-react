07b241cfe8623a11b7204ec0779165b5f1d8ba67
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-11-20 21:51:07 +0200

    fix

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,12 +1,12 @@
 import { List, merge, Map } from 'immutable';
 
 export function concatElements(page1, page2) {
-  return merge(List(page1), List(page2));
+  return List(page1).concat(List(page2));
 }
 
 export function mergeElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
   // prettier-ignore
-  return  (map1.equals(map2)) ? map2.toList() : merge(map1.toList(), map2.toList());
+  return  (map1.equals(map2)) ? map2.toList() : map1.toList().concat(map2.toList());
 }
